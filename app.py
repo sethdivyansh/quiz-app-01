04e29d255b5d7ee5c2cc363f3d7056eb6ac83885
Divyansh Seth <divyanshseth08@gmail.com>
2024-03-03 03:24:58 +0530

    ready for deployment?

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, jsonify, request, render_template
 import mysql.connector
+from mysql.connector import errorcode
 
 app = Flask(__name__)
 
-con = mysql.connector.connect(host="localhost", user="root", password = "Divyansh@08", database="quiz")
+try:
+    con = mysql.connector.connect(user="divyansh", password="23ab89DS!", host="divyansh-quiz.mysql.database.azure.com",  database="quiz")
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with the user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+    @app.route('/')
+    def error():
+        return render_template('error.html')
+else:
+  cursor = con.cursor()
+
+
 cur = con.cursor()
 cur.execute("select count(*) from questions")
 
@@ -27,4 +43,4 @@ def get_quesion():
     return jsonify(question)
 
 if __name__== '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
